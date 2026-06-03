a69bc255eb185c126d513acabb51df21a39f507b
ga6ap <onlyapencil@gmail.com>
2026-06-03 02:32:58 +0200

    Set local to v1.0 and manifest to v2.0 for testing update flow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.example.fluxsona"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "3.0"
+        versionCode = 1
+        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
